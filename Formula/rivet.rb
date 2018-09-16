94c2dc0190b7d6f0c5b83091f73791f790561633
Michele <mpapucci@me.com>
2018-09-16 15:00:32 +0200

    EOS.undent deprecation fixed

--- Formula/rivet.rb
+++ Formula/rivet.rb
@@ -57,7 +57,7 @@ class Rivet < Formula
     bash_completion.install share/"Rivet/rivet-completion"
   end
 
-  def caveats; <<-EOS.undent
+  def caveats; <<~EOS
     It may now be necessary to rebuild your Rivet analyses.
     In case of problems, check your RIVET_ANALYSIS_PATH for old analyses.
 
