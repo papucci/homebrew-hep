64c28b049634c9ffcb4a5f392e2a716a204b8685
Michele Papucci <mpapucci@caltech.edu>
2021-04-27 13:31:56 -0700

    updated madgraph5 to 3.1

--- Formula/madgraph5_amcatnlo.rb
+++ Formula/madgraph5_amcatnlo.rb
@@ -1,8 +1,8 @@
 class Madgraph5Amcatnlo < Formula
   desc "Automated LO and NLO processes matched to parton showers"
   homepage "https://launchpad.net/mg5amcnlo"
-  url "https://launchpad.net/mg5amcnlo/2.0/2.6.x/+download/MG5_aMC_v2.6.5.tar.gz"
-  sha256 "f41d1afa11566d80c867e1bf3d8d135a28e73042d30bd75f28313dee965e0bdb"
+  url "https://launchpad.net/mg5amcnlo/3.0/3.1.x/+download/MG5_aMC_v3.1.0.tar.gz"
+  sha256 "ea14cf6daa5b6c53dba1455c086e762cb8042d342451c9b88b2bde81098bf264"
 
   bottle :unneeded
 
@@ -22,45 +22,6 @@ class Madgraph5Amcatnlo < Formula
 
     # Homebrew deletes empty directories, but aMC@NLO needs them
     Dir["**/"].reverse_each { |d| touch prefix/d/".keepthisdir" if Dir.entries(d).sort==%w[. ..] }
-
-    # fix python2
-
-    inreplace prefix/"MadSpin/madspin",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"Template/MadWeight/bin/madweight.py",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"Template/MadWeight/bin/mw_options",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"Template/NLO/bin/generate_events",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"Template/NLO/bin/shower",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"Template/NLO/bin/aMCatNLO",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"Template/NLO/bin/calculate_xsect",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"Template/NLO/Utilities/NLO_Born3.py",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"Template/NLO/Utilities/VetoPrefactors/virt_reweighter.py",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"Template/NLO/Utilities/VetoPrefactors/resum_reweighter.py",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"Template/LO/bin/internal/Gridpack/gridrun",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"Template/LO/bin/generate_events",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"Template/LO/bin/madevent",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"aloha/bin/aloha",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"bin/mg5",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"bin/mg5_aMC",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"bin/.compile.py",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"madgraph/madweight/MW_driver.py",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"madgraph/interface/madgraph_interface.py",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"madgraph/various/histograms.py",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"madgraph/various/plot_djrs.py",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"madgraph/iolibs/gen_infohtml.py",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"madgraph/iolibs/template_files/loop_optimized/check_sa_all.py.inc",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"madgraph/iolibs/template_files/loop_optimized/check_sa.py.inc",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"madgraph/iolibs/template_files/loop/check_sa_all.py.inc",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"tests/parallel_tests/loop_sample_script.py",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"tests/parallel_tests/sample_script.py",  "/usr/bin/env python", "/usr/bin/env python2"
-    inreplace prefix/"Template/MadWeight/src/makefile",  "python ../", "python2 ../"
-    inreplace prefix/"Template/MadWeight/mod_file/mod_setup_model",  "python ./bin", "python2 ./bin"
-    inreplace prefix/"INSTALL",  "python .", "python2 ."
-    inreplace prefix/"tests/acceptance_tests/test_model_equivalence.py",  "call(['python','", "call(['python2','"
-    inreplace prefix/"Template/LO/bin/internal/addmasses_optional.py",  "/usr/bin/python", "/usr/bin/env python2"
-    inreplace prefix/"Template/LO/Source/.make_opts",  "python -c", "python2 -c"
-    inreplace prefix/"bin/.compile.py",  "call('python ", "call('python2 "
-    inreplace prefix/"madgraph/interface/common_run_interface.py",  "call(['python'", "call(['python2'"
-    inreplace prefix/"madgraph/various/progressbar.py",  "/usr/bin/python", "/usr/bin/env python2"
 
   end
 
